142519358202c086bcc4d46a195bcacfffb6c9c9
Karla Aldana <karla.aldana@students.dominican.edu>
2022-11-10 11:00:59 -0800

    init

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
+// app.js
 
-// Render the "home" layout for the main page and send the following message
-app.get('/', (reg, res) => {
+// Render the "home" layout for the main page and send the following msg
+app.get('/', (req, res) => {
   res.render('home', { msg: 'Handlebars are Cool!' });
 })
 
 // Initialize express
 const express = require('express')
 const app = express()
+
+// require handlebars
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+
+// Use "main" as our default layout
+app.engine('handlebars', exphbs({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
+// Use handlebars to render
+app.set('view engine', 'handlebars');
 
 // Tell our app to send the "hello world" message to our home page
 app.get('/', (req, res) => {
@@ -20,14 +31,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('App listening on port 3000!')
 })
-
-// require handlebars
-const exphbs = require('express-handlebars');
-const Handlebars = require('handlebars')
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
-
-// Use "main" as our default layout
-app.engine('handlebars', exphbs({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
-
-// Use handlebars to render
-app.set('view engine', 'handlebars');
